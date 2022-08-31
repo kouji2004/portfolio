a7cc1d043143f029bb2a9fc72f41e84c381b5e79
kouji2004 <mrtaxi48@gmail.com>
2022-08-31 10:37:09 +0000

    error_message

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Portfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.available_locales = %i[ja en]
+    # config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
